df63ca6be6799fdb0b8a2de0102faaa14943d898
Lim <justlim@amazon.com>
2018-07-06 15:37:34 -0700

    edited index and admintable for displaying db entries

--- js/sb-admin-datatables.js
+++ js/sb-admin-datatables.js
@@ -1,4 +1,31 @@
 // Call the dataTables jQuery plugin
+
+function httpGetSync(theUrl)
+{
+	var response = "";
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open("GET", theUrl, false); // true for asynchronous 
+    xmlHttp.send(null);
+    response = xmlHttp.responseText;
+    return response;
+}
+
 $(document).ready(function() {
-  $('#dataTable').DataTable();
+  response = JSON.parse(httpGetSync("https://cvvd7158f2.execute-api.us-west-2.amazonaws.com/prod/get-items").toString());
+  console.log(response.body);
+  var body = JSON.parse(response.body);
+  console.log(body.Items);
+  $('#dataTable').DataTable({
+  	"aaData": body.Items,
+  	"aoColumns": [
+        { "mDataProp": "time" },
+        { "mDataProp": "foodlevel" },
+        { "mDataProp": "URL" }
+    ],
+    "columns": [
+		{title: "time"},
+		{title: "food level"},
+		{title: "URL"}
+    ]
+  });
 });
